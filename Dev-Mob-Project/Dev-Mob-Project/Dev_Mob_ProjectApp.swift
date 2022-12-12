a9ac24fc2e6a4f80e2ca8d2c40fe39c834e3749b
Paul Ancelet <paul.ancelet@efrei.net>
2022-12-12 13:36:28 +0000

    Initial Commit

---
+++ Dev-Mob-Project/Dev-Mob-Project/Dev_Mob_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dev_Mob_ProjectApp.swift
+//  Dev-Mob-Project
+//
+//  Created by user232673 on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Dev_Mob_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dev-Mob-Project/Dev-Mob-ProjectUITests/Dev_Mob_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Dev_Mob_ProjectUITestsLaunchTests.swift
+//  Dev-Mob-ProjectUITests
+//
+//  Created by user232673 on 12/12/22.
+//
+
+import XCTest
+
+final class Dev_Mob_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
